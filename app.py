cc8e8179164845374a1165419a6ff7762d47684a
gladys mawarni <gladysmawarni@gmail.com>
2024-10-12 19:53:22 +0100

    debug

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def check_password():
 
 ### ---DATA---
 if env == 'PROD':
+    st.write('password')
     # Check password in prod, continue if password is correct
     if not check_password():
         st.stop() 
