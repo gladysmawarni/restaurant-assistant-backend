6ac42d098bafa99bd0032663b80f1aa95e8348d4
gladys mawarni <gladysmawarni@gmail.com>
2024-10-12 19:39:14 +0100

    v2

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import pandas as pd
 import time
+from datetime import datetime
+import pydeck as pdk
+# import folium
+# from streamlit_folium import st_folium
 
 
 env = 'PROD'
 
 if env == 'PROD':
-    print(env)
     # Load credentials from Streamlit secrets and verify the content structure
     try:
         cred = credentials.Certificate(dict(st.secrets["GOOGLE_CREDENTIALS"]))
@@ -36,7 +39,6 @@ elif env == 'DEV':
     # Initialize Firestore client
     db = firestore.Client()
 
-print('here1')
 # Specify the folder path
 folder_path = 'data'
 # Get the current time
@@ -52,9 +54,13 @@ for filename in os.listdir(folder_path):
     if os.path.isfile(file_path):
         # Get the file's modification time
         file_mod_time = os.path.getmtime(file_path)
+         # Convert the modification time to a readable format
+        last_updated = datetime.fromtimestamp(file_mod_time).strftime('%Y-%m-%d %H:%M:%S')
         
         # Check if the file is older than 24 hours
         if current_time - file_mod_time > time_threshold:
+            st.write(f"Data was last retrieved on {last_updated} and is older than 24 hours. Retrieving latest data...")
+
             # Fetch the data from Firestore
             rests = [i.id.strip() for i in db.collection("restaurants").get()][:10]
             addresses = [i.to_dict()['Address'] for i in db.collection("restaurants").get()][:10]
@@ -73,16 +79,81 @@ for filename in os.listdir(folder_path):
             csv_filename = str(int(time.time())) + '.csv'
             df.to_csv(os.path.join(folder_path, csv_filename), index=False)
             
-            # Display the DataFrame in Streamlit
-            st.dataframe(df[['Restaurant Name', 'Address', 'Appears on']])
         
         else:
-            st.write('here')
+            st.write(f"Data was last retrieved on {last_updated}")
             # Read the latest CSV file from the folder
             csv_path = os.path.join(folder_path, filename)
             df = pd.read_csv(csv_path)
 
 
-# Display the DataFrame in Streamlit
+### ---DATA---
 st.dataframe(df[['Restaurant', 'Address', 'Appears on']])
 st.write(f'Total restaurant in database: {len(df)}')
+
+### ---MAP---           
+# Set initial view state for the map with a specified zoom level
+view_state = pdk.ViewState(
+    latitude=51.5074,  # Latitude for London
+    longitude=-0.1278,  # Longitude for London
+    zoom=12,  # Adjust zoom level as needed
+    pitch=0
+)
+
+# Define the layer for displaying the data points
+layer = pdk.Layer(
+    'ScatterplotLayer',
+    data=df,
+    get_position='[longitude, latitude]',
+    get_radius=50,
+    get_color='[200, 30, 0, 160]',
+    pickable=True
+)
+
+# Create the map using pydeck
+map = pdk.Deck(
+    initial_view_state=view_state,
+    layers=[layer],
+    tooltip={"text": "{Restaurant}\n{Address}"}
+)
+
+st.pydeck_chart(map)
+ 
+
+
+# df_map = pd.DataFrame(data_map)
+
+# def create_map():
+#     # Create the map with Google Maps
+#     map_obj = folium.Map(tiles=None)
+#     folium.TileLayer("https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", 
+#                      attr="google", 
+#                      name="Google Maps", 
+#                      overlay=True, 
+#                      control=True, 
+#                      subdomains=["mt0", "mt1", "mt2", "mt3"]).add_to(map_obj)
+#     return map_obj
+
+# def add_markers(map_obj, locations, popup_list=None):
+#     if popup_list is  None:
+#         # Add markers for each location in the DataFrame
+#         for lat, lon in locations:
+#             folium.Marker([lat, lon]).add_to(map_obj)
+#     else:
+#         for i in range(len(locations)):
+#             lat, lon = locations[i]
+#             popup = popup_list[i]
+#             folium.Marker([lat, lon], popup=popup).add_to(map_obj)
+
+#     # Fit the map bounds to include all markers
+#     south_west = [min(lat for lat, _ in locations) - 0.02, min(lon for _, lon in locations) - 0.02]
+#     north_east = [max(lat for lat, _ in locations) + 0.02, max(lon for _, lon in locations) + 0.02]
+#     map_bounds = [south_west, north_east]
+#     map_obj.fit_bounds(map_bounds)
+
+#     return map_obj
+
+
+# m = create_map()
+# m = add_markers(m, df_map['lat_lng'], df_map['name'])
+# st_folium(m)
