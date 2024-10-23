b4dde2af54db29fff72a613a0def735a48470763
gladys mawarni <gladysmawarni@gmail.com>
2024-10-23 18:25:38 +0100

    v2

--- app.py
+++ app.py
@@ -71,52 +71,23 @@ elif env == 'DEV':
     # Initialize Firestore client
     db = firestore.Client()
 
-# Specify the folder path
-folder_path = 'data'
-# Get the current time
-current_time = time.time()
-# Define a time threshold (24 hours = 86400 seconds)
-time_threshold = 24 * 60 * 60
 
-# Iterate over the files in the folder
-for filename in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, filename)
-    
-    # Check if the path is a file (not a directory)
-    if os.path.isfile(file_path):
-        # Get the file's modification time
-        file_mod_time = os.path.getmtime(file_path)
-         # Convert the modification time to a readable format
-        last_updated = datetime.fromtimestamp(file_mod_time).strftime('%Y-%m-%d %H:%M:%S')
-        
-        # Check if the file is older than 24 hours
-        if current_time - file_mod_time > time_threshold:
-            st.write(f"Data was last retrieved on {last_updated} and is older than 24 hours. Retrieving latest data...")
+# Fetch the data from Firestore
+rests = [i.id.strip() for i in db.collection("restaurants").get()][:10]
+addresses = [i.to_dict()['Address'] for i in db.collection("restaurants").get()][:10]
+reviews = [i.to_dict()["Reviews"] for i in db.collection("restaurants").get()][:10]
+lat_long = [[i.to_dict()["Latitude"], i.to_dict()['Longitude']] for i in db.collection("restaurants").get()][:10]
 
-            # Fetch the data from Firestore
-            rests = [i.id.strip() for i in db.collection("restaurants").get()][:10]
-            addresses = [i.to_dict()['Address'] for i in db.collection("restaurants").get()][:10]
-            reviews = [i.to_dict()["Reviews"] for i in db.collection("restaurants").get()][:10]
-            lat_long = [[i.to_dict()["Latitude"], i.to_dict()['Longitude']] for i in db.collection("restaurants").get()][:10]
-            
-            # Extract review sources
-            reviews_sources = []
-            for i in reviews:
-                src = set([x['source'] for x in i])
-                reviews_sources.append(', '.join(list(src)))
+# Extract review sources
+reviews_sources = []
+for i in reviews:
+    src = set([x['source'] for x in i])
+    reviews_sources.append(', '.join(list(src)))
 
-            # Create a DataFrame and save it to a CSV file
-            data_dict = {'Restaurant Name': rests, 'Address': addresses, 'Appears on': reviews_sources, 'Lat_lng': lat_long}
-            df = pd.DataFrame(data_dict)
-            csv_filename = str(int(time.time())) + '.csv'
-            df.to_csv(os.path.join(folder_path, csv_filename), index=False)
-            
-        
-        else:
-            st.write(f"Data was last retrieved on {last_updated}")
-            # Read the latest CSV file from the folder
-            csv_path = os.path.join(folder_path, filename)
-            df = pd.read_csv(csv_path)
+# Create a DataFrame and save it to a CSV file
+data_dict = {'Restaurant Name': rests, 'Address': addresses, 'Appears on': reviews_sources, 'Lat_lng': lat_long}
+df = pd.DataFrame(data_dict)
+
 
 
 ### ---DATA---
